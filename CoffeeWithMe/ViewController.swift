ea90a4125506b45d7ddafe47151229161720a271
Jesse Lin <n823n823@gmail.com>
2016-07-08 20:41:13 +0800

    add .gitignoe

---
+++ CoffeeWithMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoffeeWithMe
+//
+//  Created by Jesselin on 2016/7/8.
+//  Copyright © 2016年 JesseLin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
